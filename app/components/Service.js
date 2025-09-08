439a458104315a518199ed9b5369b53137b42be9
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-08 18:40:36 +0200

    Made an adjustment to the service heading

---
+++ app/components/Service.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { home } from "../constants/index.js";
+import ServiceCard from "./ServiceCard";
+
+const Service = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="my-16 space-y-4">
+        
+        <h2 className="text-3xl font-bold text-center">Our Services</h2>
+        <h1 className="text-5xl text-center font-semibold ">
+          Your Favorite Cleaning & <br/> Laundry Partner
+        </h1>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+        {home.map((service, index) => {
+          return <ServiceCard key={index} service={service} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Service;
