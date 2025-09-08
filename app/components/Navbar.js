71840c275a2400fa7e1b99ac29db775422f3fd54
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-08 18:41:32 +0200

    Made an adjustment to the Navbar component

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+  
   return (
     <div className="">
       <div className="container mx-auto hidden  md:flex justify-between items-center p-4 ">
@@ -11,16 +12,16 @@ const Navbar = () => {
           <nav className="flex space-x-6 text-lg font-semibold">
             <Link href="/">Home</Link>
             <Link href="/about-us">About Us</Link>
-            <Link href="/services">Our Services</Link>
+            <Link href="/service">Our Services</Link>
             <Link href="/contact-us">Contact Us</Link>
           </nav>
         </div>
 
         <div className="flex space-x-4">
-          <button className="bg-white py-2 px-6 font-semibold text-blue-600 rounded-lg shadow-md">
+          <button className="bg-white py-2 px-6 font-semibold text-blue-600 rounded-lg shadow-md cursor-pointer hover:bg-gray-100">
             GET QUOTE
           </button>
-          <button className="bg-blue-600 py-2 px-4 font-semibold text-white rounded-lg">
+          <button className="bg-blue-600 py-2 px-4 font-semibold text-white rounded-lg shadow-md cursor-pointer hover:bg-blue-700">
             BOOK CLEANING
           </button>
         </div>
