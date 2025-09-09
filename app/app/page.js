2cc4659084a0a78b192d8aa8aebc3c44e36ef614
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-09 15:25:47 +0200

    Added new components to the home page

--- app/app/page.js
+++ app/app/page.js
@@ -1,12 +1,16 @@
 import HeroSection from "@/components/HeroSection";
 import HeaderSection from "../components/HeaderSection";
 import Service from "../components/Service";
+import BookService from "@/components/BookService";
+import CustomerExperience from "../components/CustomerExperience";
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
       <Service />
+      <BookService/>
+      <CustomerExperience/>
     </div>
   );
 }
