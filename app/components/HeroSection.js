aa6aac1f3d5f21c0251050cf42da178abf6b4f06
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-09 15:27:07 +0200

    Updated the hero section

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -4,9 +4,9 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-between py-12 px-6 md:py-20 md:px-8">
-      <div className="w-full md:w-1/2 flex flex-col items-center md:items-start justify-center space-y-6 md:space-y-10 md:pr-16 text-center md:text-left">
-        <h1 className="font-bold leading-tight text-3xl sm:text-4xl md:text-7xl">
+    <div className="container mx-auto flex flex-col-reverse md:flex-col-reverse lg:flex-row items-center justify-between py-12 px-6">
+      <div className="w-full lg:w-1/2 flex flex-col items-center md:items-start justify-center space-y-6 md:space-y-10 md:pr-16 text-center md:text-left">
+        <h1 className="font-bold leading-tight text-3xl sm:text-4xl md:text-5xl md:text-center lg:text-7xl lg:text-left">
           Spotless <span className="text-[#0A58A2]">Homes</span> &{" "}
           <span className="text-[#0A58A2]">Offices</span> Cleaning and Fresh{" "}
           <span className="text-[#0A58A2]">Laundry</span>
@@ -15,18 +15,18 @@ const HeroSection = () => {
           Enjoy the Comfort of a Fresh, Clean Home, Office and Wardrobe with Our
           Expert Services.
         </p>
-        <div className="flex flex-col sm:flex-row items-center gap-3 w-full sm:w-auto">
-          <button className="bg-blue-600 text-white py-2 px-6 rounded-lg shadow-md w-full sm:w-auto">
+        <div className="flex justify-center items-center gap-3 w-full sm:w-auto">
+          <button className="bg-[#0A58A2] text-white py-2 px-6 rounded-lg shadow-md  ">
             Get Quote
           </button>
-          <button className="bg-white text-blue-600 py-2 px-6 rounded-lg shadow-md w-full sm:w-auto hover:bg-gray-100">
+          <button className="bg-white text-[#0A58A2] py-2 px-6 rounded-lg shadow-md  sm:w-auto hover:bg-gray-100">
             Book Cleaning
           </button>
         </div>
       </div>
-      <div className="w-full md:w-1/2 flex justify-center mb-6 md:mb-0">
+      <div className="w-full lg:w-1/2 flex justify-center mb-6 md:mb-0">
         <Image
-          src={"/hero-image.png"}
+          src={"/images/hero-image.png"}
           alt="Total Touch Cleaning and Laundry Services"
           loading="lazy"
           width={800}
