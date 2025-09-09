d3b31e023f1383aab4b6dd3e4b032c6cc46fcfcb
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-09 21:48:56 +0200

    Added the footer to the root layout

--- app/app/layout.js
+++ app/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Total Touch Cleaning and Laundry Services",
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <body className=" bg-gradient-to-b from-blue-100 via-blue-50 to-white min-h-screen">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
