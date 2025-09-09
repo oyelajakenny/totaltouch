02a56be43c494a2e6178e200d3d3aef3ced490c7
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-09 16:20:35 +0200

    Added styling

--- app/components/BookServiceCard.js
+++ app/components/BookServiceCard.js
@@ -11,7 +11,7 @@ export const BookServiceCard = ({book}) => {
           className="w-full h-auto object-cover"
         />
       </div>
-      <div className="absolute bottom-10 flex flex-col gap-2 pl-5 rounded-lg  w-11/12 ">
+      <div className="absolute bottom-7 md:bottom-10 flex flex-col gap-2 pl-5 rounded-lg  w-11/12 ">
         <h3 className="text-lg font-semibold text-amber-50">{book.title}</h3>
         <button className="text-white text-sm w-fit px-3 py-2  text-center bg-[#0A58A2] rounded-lg">
           <a href={book.url}>Learn More</a>
