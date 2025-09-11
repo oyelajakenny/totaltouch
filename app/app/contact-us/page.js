94816956390266571a21f1275382b5835612c51f
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-11 12:56:24 +0200

    Implement the contact us page

--- app/app/contact-us/page.js
+++ app/app/contact-us/page.js
@@ -1,9 +1,68 @@
 import React from 'react'
+import { contact } from '@/constants/index'
 
 const page = () => {
   return (
-    <div>page</div>
-  )
+    <div className="conatiner mx-auto flex flex-col justify-start items-center">
+      <div className="flex flex-col justify-center items-center text-center my-10">
+        <h1 className="text-3xl font-semibold">Get In Touch</h1>
+        <p>
+          Ready to experience the Total Touch difference? <br></br> Contact us
+          today for a free estimate or to schedule your service.
+        </p>
+      </div>
+      <div className="container  w-full flex flex-col-reverse md:flex-col-reverse lg:flex-row items-center justify-between px-4 lg:px-40 ">
+        <div className="w-full  lg:w-1/2 m-7 grid grid-cols-2 gap-3 ">
+          {contact.map((items, index) => {
+            return (
+              <div
+                key={index}
+                className="flex justify-start items-start shadow-sm rounded-sm gap-2 p-2 lg:p-5 bg-white mb-5"
+              >
+                <div className="p-2 w-fit rounded-lg bg-blue-200 text-blue-900">
+                  {items.icon}
+                </div>
+                <div className='flex flex-col justify-start items-start w-fit'>
+                  <h1 className="font-semibold text-xs">{items.title}</h1>
+                  <h2 className="font-normal text-[12px] overflow-hidden text-wrap">{items.heading}</h2>
+                  <p className="text-xs text-gray-500">{items.description}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="bg-white p-10 rounded-md shadow-md m-5 w-full lg:w-1/2">
+          <form className="flex flex-col gap-5">
+            <input
+              type="text"
+              placeholder="Name"
+              className="p-3 rounded-md border border-gray-300 w-full"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="p-3 rounded-md border border-gray-300 w-full"
+            />
+            <input
+              type="text"
+              placeholder="Subject"
+              className="p-3 rounded-md border border-gray-300 w-full"
+            />
+            <textarea
+              placeholder="Message"
+              className="p-3 rounded-md border border-gray-300 w-full h-[150px]"
+            />
+            <button
+              type="submit"
+              className=" w-full bg-blue-500 text-white p-3 rounded-md  mx-auto"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default page
