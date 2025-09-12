b315acc4ce7815c54fc88c52d2450994bbe30354
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-12 22:05:19 +0200

    Implemented the contact us page

--- app/app/contact-us/page.js
+++ app/app/contact-us/page.js
@@ -5,20 +5,27 @@ import { contact } from "../../constants";
 const page = () => {
   return (
     <div className="conatiner mx-auto flex flex-col justify-start items-center">
-      <div className="flex flex-col justify-center items-center text-center my-10">
+      <div className="flex flex-col gap-3 justify-center items-center text-center w-full my-10">
         <h1 className="text-3xl font-semibold">Get In Touch</h1>
         <p>
           Ready to experience the Total Touch difference? <br></br> Contact us
           today for a free estimate or to schedule your service.
         </p>
+        {/* <div className="w-full h-auto">
+          <iframe
+            src="https://www.google.com/maps/d/u/0/embed?mid=192jwuPAknDQ7kl6k10TdklpmKc8aA7k&ehbc=2E312F&noprof=1"
+            width="100%"
+            height="480"
+          ></iframe>
+        </div> */}
       </div>
-      <div className="container  w-full flex flex-col-reverse md:flex-col-reverse lg:flex-row items-center justify-between px-4 lg:px-40 ">
+      <div className="container  w-full flex flex-col-reverse md:flex-col-reverse lg:flex-row items-center justify-start px-4 lg:px-40 ">
         <div className="w-full  lg:w-1/2 m-7 grid grid-cols-2 gap-3 ">
           {contact.map((items, index) => {
             return (
               <div
                 key={index}
-                className="flex justify-start items-start shadow-sm rounded-sm gap-2 p-2 lg:p-5 bg-white mb-5"
+                className="flex justify-start items-center shadow-sm rounded-sm gap-2 p-2 lg:p-5 bg-white mb-5"
               >
                 <div className="p-2 w-fit rounded-lg bg-blue-200 text-blue-900">
                   {items.icon}
@@ -57,7 +64,7 @@ const page = () => {
             />
             <button
               type="submit"
-              className=" w-full bg-blue-500 text-white p-3 rounded-md  mx-auto"
+              className=" w-full bg-[#0A58A2] text-white p-3 rounded-md  mx-auto"
             >
               Submit
             </button>
