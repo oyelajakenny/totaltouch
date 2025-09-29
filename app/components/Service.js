ee951edc1de535bc3aa1a5d562044d41be6b215a
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-30 00:16:54 +0200

    Implemented framer motion

--- app/components/Service.js
+++ app/components/Service.js
@@ -1,11 +1,18 @@
+"use client"
 import React from "react";
 import { home } from "../constants/index.js";
 import ServiceCard from "./ServiceCard";
+import { motion } from "framer-motion";
 
 const Service = () => {
   return (
-    <div className="bg-white py-10">
-      
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+      viewport={{ once: true }}
+      className="bg-white py-10"
+    >
       <div className="container mx-auto my-40 ">
         <div className="my-16 space-y-4">
           <h2 className="text-3xl font-bold text-center text-gray-600">
@@ -22,7 +29,7 @@ const Service = () => {
           })}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
