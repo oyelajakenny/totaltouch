4e4118caad0fce98a375e0d309f9892973a18b53
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-30 00:15:20 +0200

    Implemented framer motion

--- app/components/BookService.js
+++ app/components/BookService.js
@@ -1,23 +1,31 @@
+"use client"
 import React from 'react'
 import { services } from '../constants/index.js'
 import { BookServiceCard } from './BookServiceCard'
+import { motion } from "framer-motion";
+import { MdSlowMotionVideo } from 'react-icons/md';
 
 const BookService = () => {
   return (
     <div className="container mx-auto my-20">
       <div className="px-6">
-        
         <p className="font-semibold text-2xl text-[#0A58A2]">BOOK A SERVICE</p>
         <h3 className=" text-3xl md:text-5xl font-bold leading-tight">
           Cleaning & Laundry Services<br></br> That Fit Your Busy Schedule
         </h3>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10 px-6">
+      <motion.div
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 1 * 0.2 }}
+        viewport={{ once: true }}
+        className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10 px-6"
+      >
         {services.map((book) => (
           <BookServiceCard key={book.id} book={book} />
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 }
