fbfaaca1221b2f51e7962aec4670525a45b9a0a6
Oyelaja Hussein <premiumitng@gmail.com>
2025-09-29 22:15:38 +0200

    Implementing custom css animation

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -1,12 +1,31 @@
 import Image from 'next/image'
 import React from 'react'
 import { HiMiniArrowSmallRight } from "react-icons/hi2";
+import { motion } from "framer-motion";
 import Link from 'next/link';
 
+// const container = {
+//   hidden: { opacity: 0 },
+//   visible: {
+//     opacity: 1,
+//     transition: {
+//       staggerChildren: 0.3, // delay between children
+//     },
+//   },
+// };
 
+// const item = {
+//   hidden: { opacity: 0, y: 20 },
+//   visible: { opacity: 1, y: 0 },
+// };
 const HeroSection = () => {
   return (
-    <div className="container mx-auto flex flex-col-reverse md:flex-col-reverse lg:flex-row items-center justify-between py-12 px-6 animate-bounce ">
+    <motion.div
+      initial={{ opacity: 0 }} 
+      animate={{ opacity: 1 }} 
+      transition={{ duration: 1 }}
+      className="container mx-auto flex flex-col-reverse md:flex-col-reverse lg:flex-row items-center justify-between py-12 px-6 "
+    >
       <div className="w-full lg:w-1/2 flex flex-col items-center md:items-start justify-center space-y-6 md:space-y-10 md:pr-16 text-center md:text-left">
         <h1 className="font-bold  text-2xl sm:text-4xl md:text-5xl md:text-center lg:text-7xl lg:text-left">
           Spotless <span className="text-[#0A58A2]">Homes</span> &{" "}
@@ -20,8 +39,7 @@ const HeroSection = () => {
         </p>
         <div className="w-full flex justify-center lg:justify-start">
           <div className="flex justify-center items-center gap-3 w-full sm:w-auto ">
-            <Link href="/booking" >
-              
+            <Link href="/booking">
               <button className="bg-[#0A58A2] text-white flex items-center gap-2  py-2 px-8 rounded-md shadow-md  sm:w-auto hover:bg-blue-900 hover:scale-105 transition-transform cursor-pointer ">
                 Book A Service <HiMiniArrowSmallRight size={25} />
               </button>
@@ -44,7 +62,7 @@ const HeroSection = () => {
           className="w-full max-w-md md:max-w-none h-auto object-contain"
         />
       </div>
-    </div>
+    </motion.div>
   );
 }
 
