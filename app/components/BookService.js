3c9a88fe8d0e98ad5c6dc9e629a02c75d6b55b6e
Oyelaja Hussein <premiumitng@gmail.com>
2025-10-06 19:11:32 +0200

    Updated the service component

--- app/components/BookService.js
+++ app/components/BookService.js
@@ -4,23 +4,27 @@ import { services } from "../constants/index.js";
 import { BookServiceCard } from "./BookServiceCard";
 import { motion } from "framer-motion";
 import { MdSlowMotionVideo } from "react-icons/md";
-import { Link } from "next/link";
+import  Link  from "next/link";
 
 const BookService = () => {
   return (
     <div className="container mx-auto my-20">
       <div className="px-6 flex justify-between items-end">
-        <div>
+        <div className="space-y-3 ">
           <p className="font-semibold text-2xl text-[#0A58A2]">
             BOOK A SERVICE
           </p>
-          <h3 className=" text-3xl md:text-5xl font-bold leading-tight">
+          <h3 className=" text-2xl md:text-5xl font-bold leading-tight">
             Cleaning & Laundry Services<br></br> That Fit Your Busy Schedule
           </h3>
         </div>
-       
-        <button className="bg-[#0A58A2] px-5 py-2 text-sm font-semibold text-white rounded cursor-pointer">View All Services</button>
-        
+        <div className="hidden lg:flex">
+          <Link href="/our-services" target="_blank" rel="noopener noreferrer">
+            <button className=" bg-[#0A58A2] px-8 py-3 text-lg font-semibold text-white rounded-lg cursor-pointer">
+              View All Services
+            </button>
+          </Link>
+        </div>
       </div>
 
       <motion.div
@@ -34,6 +38,13 @@ const BookService = () => {
           <BookServiceCard key={book.id} book={book} />
         ))}
       </motion.div>
+      <div className="flex justify-center mt-10 lg:hidden px-6">
+        <Link href="/our-services" target="_blank" rel="noopener noreferrer">
+          <button className=" bg-[#0A58A2] px-8 py-4 text-lg font-regular text-white rounded-lg cursor-pointer">
+            View All Services
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
