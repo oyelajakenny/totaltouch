035642a43517a3844f78dc27de3333a4c077e674
Oyelaja Hussein <premiumitng@gmail.com>
2025-10-06 21:02:10 +0200

    Implemented the sitemap page

---
+++ app/app/sitemap.js
@@ -0,0 +1,41 @@
+import { MetadataRoute } from 'next'
+
+/**
+ * @returns {MetadataRoute.Sitemap}
+ */
+export default function sitemap() {
+  const baseUrl = 'https://totaltouchcleaning.com.ng'
+  
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about-us`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/our-services`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/booking`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contact-us`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+  ]
+}
